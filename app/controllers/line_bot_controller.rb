7ff94a71f633f24cfb3545e06dffb8f24f2ee775
RY2291 <prgr2498@gmail.com>
2022-01-30 21:14:41 +0900

    [fin]返信機能

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -1,7 +1,28 @@
 class LineBotController < ApplicationController
   protect_from_forgery except: [:callback]
-  def callback
 
+  def callback
+    body = request.body.read
+    signature = request.env["HTTP_X_LINE_SIGNATURE"]
+    unless client.validate_signature(body, signature)
+      error 400 do "Bad Request" end
+    end
+
+    events = client.parse_events_from(body)
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
+        end
+      end
+    end
+    head :ok
   end
 
   private
@@ -10,5 +31,13 @@ class LineBotController < ApplicationController
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
+  end
+
+  def method
+    retrun :
+  end
+
+  def reply_method
+    
   end
 end
